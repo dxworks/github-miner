e092c508d8091dbb43b9ff839f79dc5ebb63f92d
deianapatrut <deianapatrut@yahoo.com>
2021-04-28 22:01:37 +0300

    github run fields added and tests made

--- github-miner-core/src/main/kotlin/org/dxworks/githubminer/dto/response/repository/actions/run/GithubRun.kt
+++ github-miner-core/src/main/kotlin/org/dxworks/githubminer/dto/response/repository/actions/run/GithubRun.kt
@@ -2,19 +2,43 @@ package org.dxworks.githubminer.dto.response.repository.actions.run
 
 import com.google.api.client.json.GenericJson
 import com.google.api.client.util.Key
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class GithubRun : GenericJson() {
-    @Key
-    var id: Number = -1
+    @Key("id")
+    var id: Int = -1
 
-    @Key
-    var status: String? = null
+    @Key("name")
+    var name: String? = null
 
-    @Key
+    @Key("event")
     var event: String? = null
 
-    @Key("workflow_id")
-    var workflowId: Number = -1
+    @Key("conclusion")
+    var conclusion: String? = null
+
+    val duration: Long by lazy {
+        Duration.between(LocalDateTime.parse(startedAt as CharSequence?, DateTimeFormatter.ISO_OFFSET_DATE_TIME),
+            LocalDateTime.parse(completedAt as CharSequence?, DateTimeFormatter.ISO_OFFSET_DATE_TIME)).getSeconds();
+    }
+
+    @Key("created_at")
+    private var startedAt: String? = null
+    @Key("updated_at")
+    private var completedAt: String? = null
+
+    @Key("head_sha")
+    var commitId: String? = null
+
+    @Key("head_branch")
+    var branch: String? = null
+
+    @Key("url")
+    var url: String? = null
 
     var jobs: List<GithubJob> = emptyList()
+
+    override fun toString(): String = " $id: duration : $duration \n"
 }
